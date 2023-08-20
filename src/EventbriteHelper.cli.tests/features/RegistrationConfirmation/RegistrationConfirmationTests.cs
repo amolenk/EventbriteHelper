5e919ce4f922a8a38b40e45bbf3d90e4677019e1
Tom171296 <t.vandenberg.96@gmail.com>
2023-08-20 08:28:50 +0200

    update

---
+++ src/EventbriteHelper.cli.tests/features/RegistrationConfirmation/RegistrationConfirmationTests.cs
@@ -0,0 +1,11 @@
+namespace EventbriteHelper.cli.tests.features.RegistrationConfirmation;
+
+[TestClass]
+public class RegistrationConfirmationTests
+{
+    [TestMethod]
+    public void RegistrationConfirmation_should_run_on_right_param()
+    {
+        var sut = new Program();
+    }
+}
--- src/EventbriteHelper.cli/Program.cs
+++ src/EventbriteHelper.cli/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var arguments = args;
+
+if (arguments.Any())
+{
+    return 0;
+}
+else {
+    return 1;
+}
